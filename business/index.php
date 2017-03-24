06119647d56d20a7bcd50e84b05217ee44a10446
smith <smith@DESKTOP-44TOEOD>
2017-03-24 13:52:52 +0700

    Update Login and Signup

---
+++ business/index.php
@@ -0,0 +1,59 @@
+<?php
+if (!defined('BASE_PATH'))
+    exit('No direct script access allowed');
+$title = "ผู้ดูแลระบบ";
+$active = 'admin';
+$subactive = 'home';
+is_admin('home/index');
+// check condition
+if (isset($_GET['action']) && $_GET['action'] == 'list-business') {
+    $page = isset($_GET['page']) ? $_GET['page'] : 0;
+    $action = isset($_GET['action']) ? $_GET['action'] : "list";
+    $group = isset($_GET['group']) ? $_GET['group'] : '';
+    $order = isset($_GET['order']) ? $_GET['order'] : '';
+    $params = array(
+        'action' => $action,
+        'group' => $group
+    );
+    $params = http_build_query($params);
+    $userslist = get_std_list($page, $group);
+    $total = get_total($group);
+    $url = site_url('business/list-business&') . $params;
+    //var_dump($userlist);
+}
+require_once INC_PATH . 'header.php';
+?>
+
+
+<div class="container">
+    <?php include_once INC_PATH . 'submenu-business.php'; ?>
+    <div class="page-header" style="margin-top: 0px;"><h4>จัดการข้อมูลลงทะเบียนผู้ใช้ใหม่</h4></div>
+    <?php
+    show_message();
+    ?> 
+
+
+
+</div> <!-- Main contianer -->
+<?php
+require_once INC_PATH . 'footer.php';
+
+function get_business() {
+    global $db;
+    if (isset($_GET['action']) && $_GET['action'] == 'list-business') {
+        $page = isset($_GET['page']) ? $_GET['page'] : 0;
+        $action = isset($_GET['action']) ? $_GET['action'] : "list";
+        $group = isset($_GET['group']) ? $_GET['group'] : '';
+        $order = isset($_GET['order']) ? $_GET['order'] : '';
+        $params = array(
+            'action' => $action,
+            'group' => $group
+        );
+        $params = http_build_query($params);
+        $userslist = get_std_list($page, $group);
+        $total = get_total($group);
+        $url = site_url('business/list-business&') . $params;
+        //var_dump($userlist);
+    }
+//    $sql = "SELECT * FROM business LIMIT "
+}
